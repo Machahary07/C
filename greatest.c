b2467f1e759e18f9528978936b66860484e7c73f
Machahary07 <jeumachahary07@gmail.com>
2025-01-28 11:25:57 +0530

    greatest

---
+++ greatest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// find the greatest number between 3 numbers in an array [2,6,9]
+// int greatest() {
+    // int numbers = [2, 6, 9];
+    // int max = numbers[0];
+// }
+int main() {
+    int numbers[] = {2,6,9,7,8,10};
+    int max = numbers[3];
+    printf("%d", max);;
+    return 0;
+}
