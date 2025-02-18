2b3bcc1f4c307c35a98d2c2c9b30ce39ddba5c02
Machahary07 <jeumachahary07@gmail.com>
2025-02-18 09:40:38 +0530

    Updated gof3

--- gof3.c
+++ gof3.c
@@ -1,23 +1,15 @@
-// greatest of 3 numbers
 #include <stdio.h>
+
 int main() {
-    int n, i, max;
-
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-
-    int arr[n];
-    printf("Enter the elements of the array:\n");
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (b > a) {
+        a = b;
     }
-
-    max = arr[0]; // Assume the first element is the largest
-    for (i = 1; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
+    if (c > a) {
+        a = c;
     }
-    printf ("The greatest of the numbers is: %d\n", max);
+    printf("The greatest number is: %d\n", a);
     return 0;
 }
