020ee93404b7e38f0dfb95b77035daffeb9eb3bc
Machahary07 <jeumachahary07@gmail.com>
2025-03-18 15:50:55 +0530

    Finalshiii

--- Xtriangle1.c
+++ Xtriangle1.c
@@ -5,15 +5,21 @@ int main () {
     printf("n = ");
     scanf("%d",&n);
 
-    // int s = n / 2;
-        // for (int i = 1; i <= n; i += 2) {
-        // for (int j = 0; j < s; j++) printf("  ");
-        // for (int j = 0; j < i; j++) printf("X ");
-        // printf("\n");
-        // s--;
-    // }
+    printf("Enter an odd number greater than 3: ");
+    while (scanf("%d", &n) != 1 || n % 2 == 0 || n <= 3) {
+        printf("Invalid input! Enter an odd number greater than 3: ");
+        while (getchar() != '\n');
+    }
 
-    int s = 1;
+    int s = n / 2;
+        for (int i = 1; i <= n; i += 2) {
+        for (int j = 0; j < s; j++) printf("  ");
+        for (int j = 0; j < i; j++) printf("X ");
+        printf("\n");
+        s--;
+    }
+
+    s = 1;
     for (int i = n - 2; i >= 1; i -= 2) {
         for (int j = 0; j < s; j++) printf("  ");
         for (int j = 0; j < i; j++) printf("X ");
