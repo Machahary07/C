c264f2d5f21a8de81dfeef9aae8342ce176543ec
Machahary07 <jeumachahary07@gmail.com>
2025-03-25 04:12:12 +0530

    simpler primes nums 1 to 100

---
+++ pnum.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    for (int n = 7; n <= 100; n++) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                break;
+            }
+            if (i == n - 1) {
+                printf("%d ", n);
+            }
+        }
+        if (n == 2) {
+            printf("%d ", n);
+        }
+    }
+    return 0;
+}
