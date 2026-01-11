021ad577128fbdce4d13497778ebc5930d806905
Machahary07 <jeumachahary07@gmail.com>
2026-01-11 22:24:35 +0530

    twosum cpp

---
+++ Leet/1.TwoSum/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i= 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if ( nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2,7,11,15};
+    int target = 9;
+    vector<int> result = sol.twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl; //prints Indices: 0, 1
+    return 0;
+};
+
+//g++ 1.cpp -o code.exe -std=c++14; ./code.exe
