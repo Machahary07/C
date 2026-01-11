4bbe17da2a7ad5d8bc264d2befb999e25c831602
Machahary07 <jeumachahary07@gmail.com>
2026-01-11 16:08:04 +0530

    vectors

---
+++ DSA/Vectors/code1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec; //0 capacity
+    return 0;
+};
--- cpp/array.cpp
+++ cpp/array.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+    // int marks[5] = {85, 90, 78, 92, 88};
+    // cout << "Marks of the students are:" << endl;
+    // for(int i = 0; i < 5; i++) {
+        // cout << "Student " << (i + 1) << ": " << marks[i] << endl;
+    // }
+    // return 0;
+// }
+// 
+//In powershell: g++ array.cpp -o array.exe; .\array.exe
+//In cmd: g++ array.cpp -o array.exe & array.exe
+
 int main() {
-    int marks[5] = {85, 90, 78, 92, 88};
-    cout << "Marks of the students are:" << endl;
-    for(int i = 0; i < 5; i++) {
-        cout << "Student " << (i + 1) << ": " << marks[i] << endl;
+    int marks[3] = {85, 90, 78};
+    for (int i = 0; i < 3; i++) {
+        cout << marks[i] << endl;
     }
     return 0;
 }
-
-//In powershell: g++ array.cpp -o array.exe; .\array.exe
-//In cmd: g++ array.cpp -o array.exe & array.exe
