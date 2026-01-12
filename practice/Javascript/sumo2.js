725fe1ff3ce09febdc176718588ad1b96a4148f5
Machahary07 <jeumachahary07@gmail.com>
2026-01-12 20:09:44 +0530

    /practice/javascript

---
+++ practice/Javascript/sumo2.js
@@ -0,0 +1,16 @@
+var sumOfTwo = function(a, b) {
+    return a + b;
+};
+
+console.log(sumOfTwo(3, 5));
+
+//or
+
+// function sumOfTwo(a, b) {
+//     return a + b;
+// }
+
+// Cleaner version using arrow function.
+// const sumOfTwo = (a, b) => a + b;
+// Arrow functions are more concise and often preferred in modern JavaScript.
+// Leetcode preferred arrow functions for their submissions.
