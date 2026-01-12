d4dd63320bece301aa04caae4465cdd77123fb6d
Machahary07 <jeumachahary07@gmail.com>
2026-01-12 19:38:41 +0530

    practice/sumo2.cpp

--- practice/sumo2.cpp
+++ practice/sumo2.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 class Solution {
 public:
-    int sumOfTwo(int a, int b) {
+    int sumOfTwo (int a, int b) {
         return a + b;
     }
 };
 
 int main() {
-    int num1, num2;
-    cout <<"Enter two numbers: ";
-    cin >> num1 >> num2;
-    
+    Solution sol;
+    cout << "Enter two integers: ";
+    int a, b;
+    cin >> a >> b;
+    int result = sol.sumOfTwo(a, b);
+    cout << "The sum is: " << result << endl;
+    return 0;
 }
+
+//g++ sumo2.cpp -o sumo2.exe; ./sumo2.exe
