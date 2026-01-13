7c08580464cf7116aa207be0629091846084ba37
Machahary07 <jeumachahary07@gmail.com>
2026-01-13 20:34:05 +0530

    dir change

---
+++ practice/Pattern/pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int x = 1; x <= 4; x++) {
+        // cout << x << " ";
+        for (int y = 1; y <= 4; y++) {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+};
